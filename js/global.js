2b351980e7848866d29492f53e26e4eb8722e164
dejanmarkovic <dejan.markovic@rci.rogers.com>
2014-06-03 15:00:20 -0400

    contact form js functionality + html fixes

--- js/global.js
+++ js/global.js
@@ -3,6 +3,7 @@
  */
 jQuery(document).ready(function($) {
     setup_registration_form();
+    setup_contact_form();
 });
 
 function setup_registration_form()
@@ -23,18 +24,56 @@ function setup_registration_form()
                     minlength: 2,
                     required: true
                 },
-                email: {
-                    required: true,
-                    email: true
-                },
                 lname: {
                     minlength: 2,
                     required: true
+                },
+                email: {
+                    required: true,
+                    email: true
                 }
             },
             messages: {
                 enrollerKeyCode:  {
                     required: "Please enter the sponsor key of the person who directed you here."
+                }
+            },
+            highlight: function(label) {
+                jQuery(label).closest('.control-group').addClass('error');
+            },
+            success: function(label) {
+                jQuery(label)
+                    .closest('.control-group').addClass('success');
+            }
+        });
+}
+
+function setup_contact_form()
+{
+    //alert('setup_contact_form');
+    $("#submit2").click(function () {
+        alert('submit2');
+        $("#contact").submit();
+    });
+
+    $("#contact").validate(
+        {
+            rules: {
+                cfname: {
+                    minlength: 2,
+                    required: true
+                },
+                clname: {
+                    minlength: 2,
+                    required: true
+                },
+                cemail: {
+                    required: true,
+                    email: true
+                },
+                cmessage: {
+                    minlength: 2,
+                    required: true
                 }
             },
             highlight: function(label) {
